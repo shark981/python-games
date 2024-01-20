c9f6fa0a45da2ca6821fa7fbaa6ed69d2d258fbc
Leo Wu <leowu@Wus-MacBook-Air.local>
2024-01-20 15:20:29 -0500

    Add follow-the-numbers game

---
+++ follow-the-numbers/numbers.py
@@ -0,0 +1,42 @@
+from random import randint
+
+
+WIDTH = 400
+HEIGHT = 400
+
+dots = []
+lines = []
+next_dot = 0
+game_over = False # prevents the gameover screen from showing at start
+
+
+for dot in range(0,10):
+	actor = Actor("dot")
+	actor.pos = randint(20, WIDTH - 20), randint(20, HEIGHT - 20)
+	dots.append(actor)
+
+def draw():
+	screen.fill("black")
+	number = 1 
+	for dot in dots:
+		screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))
+		dot.draw() 
+		number = number + 1
+	for line in lines:
+		screen.draw.line(line[0], line[1], (100, 0, 0))
+	if game_over:
+		screen.fill("blue") # Change background to pink.
+		screen.draw.text("game over! :( ", topleft=(10,60), fontsize=60) 
+
+def on_mouse_down(pos):
+	global next_dot
+	global lines
+	global game_over
+	if dots[next_dot].collidepoint(pos):
+		if next_dot:
+			lines.append((dots[next_dot - 1].pos, dots[next_dot].pos))
+		next_dot = next_dot + 1
+	else:
+		game_over = True
+		# lines = []
+		# next_dot = 0
