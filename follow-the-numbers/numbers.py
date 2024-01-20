c0ac7bed7965e7187dff4857bd2ce132bbda29ba
Leo Wu <leowu@Wus-MacBook-Air.local>
2024-01-20 16:39:22 -0500

    add coin-collector

--- follow-the-numbers/numbers.py
+++ follow-the-numbers/numbers.py
@@ -1,5 +1,9 @@
 from random import randint
 
+from time import time
+
+def update ():
+	pass
 
 WIDTH = 400
 HEIGHT = 400
@@ -8,7 +12,9 @@ dots = []
 lines = []
 next_dot = 0
 game_over = False # prevents the gameover screen from showing at start
+succeed_time = 0
 
+time_start = time()
 
 for dot in range(0,10):
 	actor = Actor("dot")
@@ -26,16 +32,24 @@ def draw():
 		screen.draw.line(line[0], line[1], (100, 0, 0))
 	if game_over:
 		screen.fill("blue") # Change background to pink.
-		screen.draw.text("game over! :( ", topleft=(10,60), fontsize=60) 
+		screen.draw.text("game over! :( ", topleft=(10,60), fontsize=60)
+	else:
+		if not succeed_time:
+			screen.draw.text("time: " + str(time() - time_start), (5, 5))
+		else:
+			screen.draw.text("You succeeded! time: " + str(succeed_time - time_start), (5, 5))
 
 def on_mouse_down(pos):
 	global next_dot
 	global lines
 	global game_over
+	global succeed_time
 	if dots[next_dot].collidepoint(pos):
 		if next_dot:
 			lines.append((dots[next_dot - 1].pos, dots[next_dot].pos))
 		next_dot = next_dot + 1
+		if next_dot == 10:
+			succeed_time = time()
 	else:
 		game_over = True
 		# lines = []
